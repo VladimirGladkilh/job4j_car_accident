80382f4396463b68f2b232f85d69585b80ba15e5
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2021-04-18 22:31:44 +0500

    1. Загрузка данных в вид[295782#357525]

--- src/main/java/accident/control/IndexController.java
+++ src/main/java/accident/control/IndexController.java
@@ -5,10 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("user", "Владимир Гладких");
+        List<String> list = List.of("Первый элемент", "Второй элемент", "Третий эдемент", "Четвертый элемент", "Ева Даллас");
+        model.addAttribute("stringList", list);
         return "index";
     }
 }
