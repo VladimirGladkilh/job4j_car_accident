058005e5aa86ee01929297c23375d49ad6ae1c1f
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2021-05-26 00:10:47 +0500

    0. Spring DataSource

---
+++ src/main/java/accident/db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE accident
+(
+    id      serial primary key,
+    name    varchar(2000),
+    text    varchar(2000),
+    address varchar(2000),
+    type_id int
+);
---
+++ src/main/java/accident/repository/AccidentJdbcTemplate.java
@@ -0,0 +1,70 @@
+package accident.repository;
+
+import accident.model.Accident;
+import accident.model.AccidentType;
+import accident.model.Rule;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+public class AccidentJdbcTemplate implements Store {
+    private final JdbcTemplate jdbc;
+
+    public AccidentJdbcTemplate(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+
+    @Override
+    public Collection<Accident> findAll() {
+        return jdbc.query("select id, name from accident",
+                (rs, row) -> {
+                    Accident accident = new Accident();
+                    accident.setId(rs.getInt("id"));
+                    accident.setName(rs.getString("name"));
+                    return accident;
+                });
+    }
+
+    @Override
+    public void save(Accident accident) {
+
+    }
+
+    @Override
+    public Accident findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Accident accident) {
+
+    }
+
+    @Override
+    public AccidentType findTypeById(int id) {
+        return null;
+    }
+
+    @Override
+    public Collection<AccidentType> findAllTypes() {
+        return null;
+    }
+
+    @Override
+    public Rule findRuleById(int id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Rule> findAllRules() {
+        return null;
+    }
+
+    @Override
+    public Collection<Rule> findRulesByAccidientId(int id) {
+        return null;
+    }
+}
